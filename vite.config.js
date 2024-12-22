52b5be5123c88110145da8d16e98d3524f89ce8f
Sebnem <sebkay2002@hotmail.com>
2024-12-22 16:48:19 +0300

    Update vite.config.js
    den

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      output: { manualChunks: { swc: ["@swc/wasm", "@swc/core"] } },
-    },
+      output: {
+        manualChunks: {
+          swc: ['@swc/wasm', '@swc/core']
+        }
+      }
+    }
   },
+  assetsInclude: ['**/*.node']
 });
